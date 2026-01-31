a5d7f293d9322d3b5a67046ee4a242449a0bfc65
Ale <55502408+ItsDasp@users.noreply.github.com>
2026-01-31 02:22:49 -0300

    Actualizar main.js

--- src/main.js
+++ src/main.js
@@ -1,89 +1,35 @@
-const hugGifs = require('./gifs/hugs');
-const kissGifs = require('./gifs/kisses');
-const slapGifs = require('./gifs/slaps');
-const pokeGifs = require('./gifs/pokes');
-const patGifs = require('./gifs/pats');
-const cuddleGifs = require('./gifs/cuddles');
-const ticklesGifs = require('./gifs/tickles')
-const smugGifs = require('./gifs/smugs')
-const biteGifs = require('./gifs/bite')
-const feedGifs = require('./gifs/feed')
-const highFiveGifs = require('./gifs/highfive')
-
-
-module.exports = {
-    hug: function() {
-        const randomIndex = Math.floor(Math.random() * hugGifs.length);
-        return hugGifs[randomIndex];
-    },
-    poke: function() {
-        const randomIndex = Math.floor(Math.random() * pokeGifs.length);
-        return pokeGifs[randomIndex];
-    },
-    kiss: function() {
-        const randomIndex = Math.floor(Math.random() * kissGifs.length);
-        return kissGifs[randomIndex];
-    },
-    slap: function() {
-        const randomIndex = Math.floor(Math.random() * slapGifs.length);
-        return slapGifs[randomIndex];
-    },
-    pat: function() {
-        const randomIndex = Math.floor(Math.random() * patGifs.length);
-        return patGifs[randomIndex];
-    },
-    cuddle: function() {
-        const randomIndex = Math.floor(Math.random() * cuddleGifs.length);
-        return cuddleGifs[randomIndex];
-    },
-    tickle: function() {
-        const randomIndex = Math.floor(Math.random() * ticklesGifs.length);
-        return ticklesGifs[randomIndex];
-    },
-    smug: function() {
-        const randomIndex = Math.floor(Math.random() * smugGifs.length);
-        return ticklesGifs[randomIndex];
-    },
-    bite: function() {
-        const randomIndex = Math.floor(Math.random() * biteGifs.length);
-        return biteGifs[randomIndex];
-    },
-    feed: function() {
-        const randomIndex = Math.floor(Math.random() * feedGifs.length);
-        return feedGifs[randomIndex];
-    },
-    highfive: function() {
-        const randomIndex = Math.floor(Math.random() * highFiveGifs.length);
-        return highFiveGifs[randomIndex];
-    },
-
-    allGifs: function() {
-        const totalHugGifs = hugGifs.length;
-        const totalKissGifs = kissGifs.length;
-        const totalSlapGifs = slapGifs.length;
-        const totalPokeGifs = pokeGifs.length;
-        const totalPatGifs = patGifs.length;
-        const totalCuddleGifs = cuddleGifs.length;
-        const totalTicklesGifs = ticklesGifs.length;
-        const totalSmugsGifs = smugGifs.length;
-        const totalBiteGifs = biteGifs.length;
-        const totalFeedGifs = feedGifs.length;
-        const totalHighFiveGifs = highFiveGifs.length;
-        const totalGifs = totalHugGifs + totalKissGifs + totalSlapGifs + totalPokeGifs + totalPatGifs + totalCuddleGifs + totalTicklesGifs + totalSmugsGifs + totalBiteGifs + totalFeedGifs + totalHighFiveGifs;
-
-        return {
-            totalHugGifs,
-            totalKissGifs,
-            totalSlapGifs,
-            totalPokeGifs,
-            totalPatGifs,
-            totalCuddleGifs,
-            totalTicklesGifs,
-            totalSmugsGifs,
-            totalBiteGifs,
-            totalFeedGifs,
-            totalHighFiveGifs,
-            totalGifs
-        };
-    }
+const gifs = {
+    hug: require('./gifs/hugs'),
+    kiss: require('./gifs/kisses'),
+    slap: require('./gifs/slaps'),
+    poke: require('./gifs/pokes'),
+    pat: require('./gifs/pats'),
+    cuddle: require('./gifs/cuddles'),
+    tickle: require('./gifs/tickles'),
+    smug: require('./gifs/smugs'),
+    bite: require('./gifs/bite'),
+    feed: require('./gifs/feed'),
+    highfive: require('./gifs/highfive')
 };
+
+const randomFrom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const api = Object.fromEntries(
+    Object.entries(gifs).map(([key, value]) => [key, () => randomFrom(value)])
+);
+
+api.allGifs = () => {
+    const totals = {};
+    let totalGifs = 0;
+
+    for (const [key, value] of Object.entries(gifs)) {
+        const count = value.length;
+        totals[`total${key.charAt(0).toUpperCase() + key.slice(1)}Gifs`] = count;
+        totalGifs += count;
+    }
+
+    totals.totalGifs = totalGifs;
+    return totals;
+};
+
+module.exports = api;
